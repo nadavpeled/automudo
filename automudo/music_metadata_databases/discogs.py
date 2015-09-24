4ff4467afbe9abb3bb77ba272184f02a5d8980a9
nadavpeled <nadav.peled93@gmail.com>
2015-09-24 16:07:59 +0300

    Add throttling to the Discogs client

--- automudo/music_metadata_databases/discogs.py
+++ automudo/music_metadata_databases/discogs.py
@@ -4,6 +4,7 @@ import datetime
 import requests
 
 from .base import MusicMetadata, TrackMetadata, MusicMetadataDatabase
+from ..utils.throttle import throttle
 
 
 class DiscogsMetadataDatabase(MusicMetadataDatabase):
@@ -46,6 +47,83 @@ class DiscogsMetadataDatabase(MusicMetadataDatabase):
         else:
             return 1
 
+    @throttle(seconds=3)
+    def _get_release_details(self, resource_url, formats=None):
+        """
+            Gets the release details from the given resource URL.
+            Note that the formats field will not be field
+            unless you provide it in the formats argument.
+        """
+        headers = {'User-Agent': self.__user_agent}
+        album_details = requests.get(
+            resource_url, headers=headers
+            ).json()
+
+        artist = ""
+        last_join = ""
+        for single_artist in album_details['artists']:
+            if last_join:
+                if re.match(r"\w", last_join[0]):
+                    artist += " "
+                artist += "{} ".format(last_join)
+            artist += single_artist['name']
+            last_join = single_artist['join']
+
+        # Remove the string ", the" from the artist name.
+        i = artist.lower().find(', the')
+        if i > 0:
+            artist = artist[:i]
+
+        # Remove "The " from the beginning of the artist name.
+        if artist.lower().startswith('the '):
+            artist = artist.partition('the ')[2]
+
+        # Discogs distinguishes between multiple artists
+        # with the same name by writing a numeric identifer
+        # in parenthesis after the artist name.
+        # We don't need this, so we omit it.
+        artist = re.sub(r"\s*\([0-9]+\)$", "", artist)
+
+        title = album_details['title']
+        # When albums with parenthesis in their names are
+        # referenced online, the parenthesis part is usually omitted.
+        # Therefore, if we don't omit the parenthesis part,
+        # searches for albums by the metadata we provide
+        # might not be as successful.
+        title = re.sub(r"\([^\)]*\)", "", title)
+        title = re.sub(r"\[[^\]]*\]", "", title)
+        title = re.sub(r"\{[^\}]*\}", "", title)
+
+        tracks = []
+        for track in album_details.get('tracklist', []):
+            if track['duration']:
+                minute, second = map(int, track['duration'].split(':'))
+                hour = int(minute / 60)
+                minute = minute % 60
+                duration = datetime.time(hour=hour,
+                                         minute=minute,
+                                         second=second)
+            else:
+                duration = None
+
+            tracks.append(TrackMetadata(title=track['title'],
+                                        duration=duration))
+
+        release_date = album_details.get('released', None)
+        if release_date:
+            release_date = [max(1, int(x))
+                            for x in release_date.split("-")]
+            while len(release_date) < 3:
+                release_date.append(1)  # Fictive month/day.
+            release_date = datetime.date(*release_date)
+        return MusicMetadata(artist=artist, title=title,
+                             genres=album_details.get('styles', None),
+                             date=release_date,
+                             formats=formats,
+                             release_id=album_details['id'],
+                             metadata_database_name=self.name,
+                             tracks=tracks)
+
     def _find_album(self, search_string, master_releases, max_results):
         """
             Implementation for MusicMetadataDatabase._find_album .
@@ -69,71 +147,6 @@ class DiscogsMetadataDatabase(MusicMetadataDatabase):
             )
 
         for result in search_results:
-            album_details = requests.get(
-                result['resource_url'], headers=headers
-                ).json()
-
-            artist = ""
-            last_join = ""
-            for single_artist in album_details['artists']:
-                if last_join:
-                    if re.match(r"\w", last_join[0]):
-                        artist += " "
-                    artist += "{} ".format(last_join)
-                artist += single_artist['name']
-                last_join = single_artist['join']
-
-            # Remove the string ", the" from the artist name.
-            i = artist.lower().find(', the')
-            if i > 0:
-                artist = artist[:i]
-
-            # Remove "The " from the beginning of the artist name.
-            if artist.lower().startswith('the '):
-                artist = artist.partition('the ')[2]
-
-            # Discogs distinguishes between multiple artists
-            # with the same name by writing a numeric identifer
-            # in parenthesis after the artist name.
-            # We don't need this, so we omit it.
-            artist = re.sub(r"\s*\([0-9]+\)$", "", artist)
-
-            title = album_details['title']
-            # When albums with parenthesis in their names are
-            # referenced online, the parenthesis part is usually omitted.
-            # Therefore, if we don't omit the parenthesis part,
-            # searches for albums by the metadata we provide
-            # might not be as successful.
-            title = re.sub(r"\([^\)]*\)", "", title)
-            title = re.sub(r"\[[^\]]*\]", "", title)
-            title = re.sub(r"\{[^\}]*\}", "", title)
-
-            tracks = []
-            for track in album_details.get('tracklist', []):
-                if track['duration']:
-                    minute, second = map(int, track['duration'].split(':'))
-                    hour = int(minute / 60)
-                    minute = minute % 60
-                    duration = datetime.time(hour=hour,
-                                             minute=minute,
-                                             second=second)
-                else:
-                    duration = None
-
-                tracks.append(TrackMetadata(title=track['title'],
-                                            duration=duration))
-
-            release_date = album_details.get('released', None)
-            if release_date:
-                release_date = [max(1, int(x))
-                                for x in release_date.split("-")]
-                while len(release_date) < 3:
-                    release_date.append(1)  # Fictive month/day.
-                release_date = datetime.date(*release_date)
-            yield MusicMetadata(artist=artist, title=title,
-                                genres=album_details.get('styles', None),
-                                date=release_date,
-                                formats=result.get('format', None),
-                                release_id=album_details['id'],
-                                metadata_database_name=self.name,
-                                tracks=tracks)
+            yield self._get_release_details(
+                result['resource_url'], result.get('format', None)
+                )
